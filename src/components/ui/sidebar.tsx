ea98b1cca059efc6145cdeee0860bb228910db98
Abdelhamid Mosaad <abdelhamid.mosaad@gmail.com>
2025-09-04 22:41:22 +0000

    Solve

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -358,7 +358,10 @@ const SidebarHeader = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="header"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn(
+        "flex flex-col gap-2 p-2 group-data-[collapsible=icon]:items-center [&_span]:group-data-[collapsible=icon]:hidden",
+        className
+      )}
       {...props}
     />
   )
