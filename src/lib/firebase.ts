c622b0155ccb9fffed4e6527db110e2d118be2a3
Abdelhamid Mosaad <abdelhamid.mosaad@gmail.com>
2025-09-05 11:37:21 +0000

    1/2
    Next.js 15.3.3 (stale)
    Turbopack
    Runtime Error
    
    FirebaseError: Fai

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp, getApps, getApp} from 'firebase/app';
 import {getAuth} from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore';
+import {getFirestore, enablePersistence, initializeFirestore} from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -17,6 +17,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
-const db = getFirestore(app);
+const db = initializeFirestore(app, {
+    localCache: {
+        kind: 'persistent'
+    }
+});
+
 
 export {app, auth, db};
