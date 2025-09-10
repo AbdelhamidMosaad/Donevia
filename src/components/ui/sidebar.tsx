8dbc60a3b5b0f73029b87336c1f489cbf6596e94
Abdelhamid Mosaad <abdelhamid.mosaad@gmail.com>
2025-09-10 09:47:33 +0000

    Ensure by pressing this icon the sidebar will collapse or expand accrodi

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -26,7 +26,6 @@ const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
 const SIDEBAR_WIDTH = "16rem"
 const SIDEBAR_WIDTH_MOBILE = "18rem"
-const SIDEBAR_WIDTH_ICON = "3.5rem"
 const SIDEBAR_KEYBOARD_SHORTCUT = "b"
 
 type SidebarContext = {
@@ -133,8 +132,6 @@ const SidebarProvider = React.forwardRef<
             style={
               {
                 "--sidebar-width": SIDEBAR_WIDTH,
-                "--sidebar-width-icon": SIDEBAR_WIDTH_ICON,
-                ...style,
               } as React.CSSProperties
             }
             className={cn(
