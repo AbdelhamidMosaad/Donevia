542d1325b62985373efb66fdfdd692365b38e80d
Abdelhamid Mosaad <abdelhamid.mosaad@gmail.com>
2025-09-14 18:20:23 +0000

    MOve this column of icons again to the right

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -480,14 +480,21 @@ SidebarGroupContent.displayName = "SidebarGroupContent"
 const SidebarMenu = React.forwardRef<
   HTMLUListElement,
   React.ComponentProps<"ul">
->(({ className, ...props }, ref) => (
-  <ul
-    ref={ref}
-    data-sidebar="menu"
-    className={cn("flex w-full min-w-0 flex-col gap-1", className)}
-    {...props}
-  />
-))
+>(({ className, ...props }, ref) => {
+    const { open } = useSidebar();
+    return (
+    <ul
+        ref={ref}
+        data-sidebar="menu"
+        className={cn(
+            "flex w-full min-w-0 flex-col gap-1", 
+            !open ? 'pl-2' : 'pl-1',
+            className
+        )}
+        {...props}
+    />
+    )
+})
 SidebarMenu.displayName = "SidebarMenu"
 
 const SidebarMenuItem = React.forwardRef<
