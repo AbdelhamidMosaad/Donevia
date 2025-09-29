f09d0dc2381d7213869361d146777f03c44a2b6c
Abdelhamid Mosaad <abdelhamid.mosaad@gmail.com>
2025-09-29 17:36:23 +0000

    I created a new mind map and also I got error  404

---
+++ src/app/(app)/mind-map/[mindMapId]/page.tsx
@@ -0,0 +1,21 @@
+
+'use client';
+
+import dynamic from 'next/dynamic';
+import { Skeleton } from '@/components/ui/skeleton';
+
+const MindMapTool = dynamic(
+  () => import('@/components/mind-map/mind-map-tool'),
+  { 
+    ssr: false,
+    loading: () => (
+        <div className="w-full h-full p-4">
+            <Skeleton className="w-full h-full" />
+        </div>
+    )
+  }
+);
+
+export default function MindMapCanvasPage() {
+  return <MindMapTool />;
+}
