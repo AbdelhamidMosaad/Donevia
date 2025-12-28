17d805ac168d0fdbaa4db68e578cb18e08064e30
Abdelhamid Mosaad <abdelhamid.mosaad@gmail.com>
2025-12-28 15:12:09 +0000

    Solve the error related to the new logo caused problems on the following

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -18,15 +18,17 @@ export function DoneviaLogo({ className }: { className?: string }) {
           x2="1"
           y2="1"
         >
-          <stop offset="0%" stopColor="#4A00E0" />
-          <stop offset="50%" stopColor="#8E2DE2" />
-          <stop offset="100%" stopColor="#43D4C4" />
+          <stop offset="0%" stopColor="#8E2DE2" />
+          <stop offset="100%" stopColor="#4A00E0" />
         </linearGradient>
       </defs>
-      <path
-        d="M20 3H4C3.44772 3 3 3.44772 3 4V20C3 20.5523 3.44772 21 4 21H20C20.5523 21 21 20.5523 21 20V4C21 3.44772 20.5523 3 20 3Z"
-        fill="url(#logoGradient)"
-        rx="6"
+      <rect 
+        x="3" 
+        y="3" 
+        width="18" 
+        height="18" 
+        rx="4" 
+        fill="url(#logoGradient)" 
       />
       <path
         d="M6.66663 12.8704L9.91663 16.1204L17.0104 9.02609"
